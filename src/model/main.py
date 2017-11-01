c5c78e4063054b64c5d14de29b691e57d27e0542
Donald Trump <dapxichlo@Donalds-MacBook-Pro.local>
2017-11-01 16:54:04 +0700

    increase epoches

--- src/model/main.py
+++ src/model/main.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     cat_dim = (10,)
     noise_dim = (60,)
     batch_size = 32
-    n_batch_per_epoch = 100
-    nb_epoch = 20
+    n_batch_per_epoch = 2000
+    nb_epoch = 1000
     #start to train
     train.train(cat_dim,noise_dim,batch_size,n_batch_per_epoch,nb_epoch)
