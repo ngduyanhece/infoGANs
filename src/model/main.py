1a0492069ae0943e937f8518e8c1f1490be5b918
Donald Trump <dapxichlo@Donalds-MacBook-Pro.local>
2017-11-03 13:09:55 +0700

    add supervised learning

--- src/model/main.py
+++ src/model/main.py
@@ -2,8 +2,8 @@ import train
 if __name__ == "__main__":
     cat_dim = (10,)
     noise_dim = (60,)
-    batch_size = 32
-    n_batch_per_epoch = 2000
+    batch_size = 64
+    n_batch_per_epoch = 500
     nb_epoch = 1000
     #start to train
     train.train(cat_dim,noise_dim,batch_size,n_batch_per_epoch,nb_epoch)
--- src/model/train.py
+++ src/model/train.py
@@ -6,6 +6,7 @@ from keras.utils import generic_utils
 from keras.optimizers import Adam, SGD
 # Utils
 sys.path.append("../utils")
+import numpy as np
 import general_utils
 import data_utils
 
@@ -21,7 +22,10 @@ def train(cat_dim,noise_dim,batch_size,n_batch_per_epoch,nb_epoch,dset="mnist"):
     # Load and rescale data
     if dset == "mnist":
         print("loading mnist data")
-        X_real_train, _, _, _ = data_utils.load_mnist()
+        X_real_train, Y_real_train, X_real_test, Y_real_test = data_utils.load_mnist()
+        # pick 1000 sample for testing
+        # X_real_test = X_real_test[-1000:]
+        # Y_real_test = Y_real_test[-1000:]
 
     img_dim = X_real_train.shape[-3:]
     epoch_size = n_batch_per_epoch * batch_size
@@ -60,41 +64,43 @@ def train(cat_dim,noise_dim,batch_size,n_batch_per_epoch,nb_epoch,dset="mnist"):
             batch_counter = 1
             start = time.time()
 
-            for X_real_batch in data_utils.gen_batch(X_real_train, batch_size):
+            for X_real_batch,Y_real_batch in zip(data_utils.gen_batch(X_real_train, batch_size),data_utils.gen_batch(Y_real_train, batch_size)):
 
                 # Create a batch to feed the discriminator model
-                X_disc, y_disc, y_cat = data_utils.get_disc_batch(X_real_batch, generator_model, batch_counter, batch_size, cat_dim, noise_dim)
+                X_disc, y_disc = data_utils.get_disc_batch(X_real_batch,Y_real_batch, generator_model, batch_counter, batch_size, cat_dim, noise_dim)
 
                 # Update the discriminator
-                disc_loss = discriminator_model.train_on_batch(X_disc, [y_disc, y_cat])
+                disc_loss = discriminator_model.train_on_batch(X_disc, [y_disc, Y_real_batch])
 
                 # Create a batch to feed the generator model
-                X_noise, y_gen, X_cat = data_utils.get_gen_batch(batch_size, cat_dim, noise_dim)
+                X_noise, y_gen = data_utils.get_gen_batch(batch_size, cat_dim, noise_dim)
 
                 # Freeze the discriminator
                 discriminator_model.trainable = False
-                gen_loss = DCGAN_model.train_on_batch([X_cat, X_noise], [y_gen, X_cat])
+                gen_loss = DCGAN_model.train_on_batch([Y_real_batch, X_noise], [y_gen, Y_real_batch])
                 # Unfreeze the discriminator
                 discriminator_model.trainable = True
-
+                # training validation
+                _, p_Y_train = discriminator_model.predict(X_real_batch, batch_size=batch_size)
+                acc_train = data_utils.accuracy(p_Y_train, Y_real_train)
                 batch_counter += 1
                 progbar.add(batch_size, values=[("D tot", disc_loss[0]),
-                                                ("D log", disc_loss[1]),
                                                 ("D cat", disc_loss[2]),
                                                 ("G tot", gen_loss[0]),
-                                                ("G log", gen_loss[1]),
-                                                ("G cat", gen_loss[2])])
+                                                ("G cat", gen_loss[2]),
+                                                ("Q acc", acc_train)])
 
                 # Save images for visualization
                 if batch_counter % (n_batch_per_epoch / 2) == 0:
                     data_utils.plot_generated_batch(X_real_batch, generator_model, batch_size, cat_dim, noise_dim,e)
-
                 if batch_counter >= n_batch_per_epoch:
                     break
 
             print("")
             print('Epoch %s/%s, Time: %s' % (e + 1, nb_epoch, time.time() - start))
-
+            _, p_Y_test = discriminator_model.predict(X_real_test,batch_size=X_real_test.shape[0])
+            acc_test = data_utils.accuracy(p_Y_test, Y_real_test)
+            print("Epoch: {} Accuracy: {}".format(e +1 , acc_test))
             # if e % 5 == 0:
             #     gen_weights_path = os.path.join('../../models/IG/gen_weights_epoch%s.h5' % (e))
             #     generator_model.save_weights(gen_weights_path, overwrite=True)
