eca5a97cb892864fe423ba44f4df4f392e09bc9b
Donald Trump <dapxichlo@Donalds-MacBook-Pro.local>
2017-11-04 12:55:54 +0700

    adjust number of batch per epoch

--- src/model/main.py
+++ src/model/main.py
@@ -2,8 +2,8 @@ import train
 if __name__ == "__main__":
     cat_dim = (10,)
     noise_dim = (100,)
-    batch_size = 32
-    n_batch_per_epoch = 100
+    batch_size = 64
+    n_batch_per_epoch = 1000
     nb_epoch = 1000
     #start to train
     train.train(cat_dim,noise_dim,batch_size,n_batch_per_epoch,nb_epoch)
